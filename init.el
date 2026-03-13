3ca4d70418d5d3bef9b53c6f5e0a5c7b76cd9c67
Ivan <128897700+Bakamashine@users.noreply.github.com>
2026-03-13 13:08:11 +0300

    Update init.el

--- init.el
+++ init.el
@@ -1,18 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(package-selected-packages '(vertico undo-tree php-mode ace-window company))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight regular :height 158 :width normal)))))
-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
@@ -20,6 +5,7 @@
 (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
 
 (package-initialize)
+
 
 ;; Global mappings
 (global-set-key (kbd "<C-up>") 'shrink-window)
@@ -30,7 +16,11 @@
 (global-unset-key "\C-xf")
 (global-set-key [f8] 'eshell)
 
-;; Configure
+(global-set-key [f6] 'neotree)
+(global-set-key [f5] 'find-file-in-project)
+(global-set-key (kbd "C-'") #'imenu-list-smart-toggle)
+
+
 (setq make-backup-files nil)		;Отключение создания бекапа файла
 (setq auto-save-default nil)		;Отключения автосохранения
 (setq auto-save-list-file-name nil)	;Отключение именования автосохранённого файла
@@ -59,16 +49,6 @@
               aw-scope 'frame)
   :bind (("\C-xo" . ace-window))
   )
-
-;; Company-mode
-(global-company-mode)
-
-;; Autopair
-;; (add-to-list 'load-path "~/.emacs.d/plugins/")
-;; (require 'autopair)
-;; (autopair-global-mode)			
-
-;;Undo-tree
 (use-package undo-tree
 	     :ensure t
 	     :init (global-undo-tree-mode))
@@ -78,3 +58,66 @@
   :ensure t
   :init (vertico-mode)
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auctex phps-mode company lsp-ui lsp-mode imenu-list find-file-in-project neotree magit vertico undo-tree ace-window php-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Magit
+(use-package magit
+  :ensure t
+  )
+
+(use-package lsp-mode
+  :ensure t
+  :hook
+  ((c-mode . lsp)
+   (c++-mode . lsp))
+  :config
+  (setq lsp-prefer-flymake nil)  ;; Использовать lsp-ui вместо flymake
+)
+
+
+;; Company-mode configuration
+(use-package company
+  :defer t
+  :init
+  (add-hook 'text-mode-hook #'company-mode)
+  (add-hook 'latex-mode-hook #'company-mode)
+  (add-hook 'LaTeX-mode-hook #'company-mode) ;; for AUCTeX users
+  :config
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.2
+        company-show-numbers t
+        company-tooltip-align-annotations t)
+  (define-key company-active-map (kbd "TAB") #'company-complete-selection)
+  (define-key company-active-map (kbd "<tab>") #'company-complete-selection))
+(add-to-list 'auto-mode-alist '("\\.tex\\'" . latex-mode))
+(add-hook 'text-mode-hook
+          (lambda ()
+            (when (and buffer-file-name
+                       (string-match-p "\\.txt\\'" buffer-file-name))
+              (company-mode 1))))
+
+;; (use-package company
+;;   :ensure t
+;;   :init
+;;   (add-hook 'text-mode-hook #'company-mode)
+;;   (add-hook 'latex-mode-hook #'company-mode)
+;;   (add-hook 'LaTeX-mode-hook #'company-mode) ;; for AUCTeX users
+;;   :hook (lsp-mode . company-mode)  ;; Включить company-mode при запуске lsp-mode
+;;   :config
+;;   (setq company-minimum-prefix-length 1)
+;;   (setq company-idle-delay 0.0)
+
+;;   )
+(load-theme 'deeper-blue)
